ee79b38b00b8316b0360134254e4495d62a6748a
ekamjaw <kamiljaworski7@gmail.com>
2017-11-22 15:12:27 +0100

    lists and dictionaries

---
+++ Lessons/Dictionaries.py
@@ -0,0 +1,5 @@
+my_dict = {'key':'value', 'key2':'value2'}
+print(my_dict)
+
+for k in my_dict.keys():
+    print(my_dict[k])
--- Lessons/List.py
+++ Lessons/List.py
@@ -18,3 +18,22 @@ l.append('append') # add at end of list new item
 print(l)
 l.pop() #delete last item forom list, pop
 print(l)
+
+#reverse
+l.reverse()
+print(l)
+
+#sort
+l.sort()
+print(l)
+
+l_1 = [1,2,3]
+l_2 = [4,5,6]
+l_3 = [7,8,9]
+
+# list two dimensional
+matrix = [l_1,l_2,l_3]
+print(matrix)
+
+firts_col = [row[0] for row in matrix]
+print(firts_col)
